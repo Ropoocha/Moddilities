9c52c2cc2bb39e2df84952f33b109e2c4ddec9c0
Ropoocha <patapon.play@gmail.com>
2020-08-03 21:03:22 +0200

    Modify TileEntity to represent an inventory
    Added neccessary methods to read and write to TileEntity, created
    a inventory handler with one slot which accepts only diamonds.
    Also exposed inventory handler as a capability.

--- src/main/java/com/ropoocha/moddilities/blocks/BlockDiamondGenerator.java
+++ src/main/java/com/ropoocha/moddilities/blocks/BlockDiamondGenerator.java
@@ -7,26 +7,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
 
 public class BlockDiamondGenerator extends Block {
 
   public BlockDiamondGenerator() {
     super(Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(2.0f));
-  }
-
-  public static Direction getFacingFromPlacer(BlockPos pos, LivingEntity placer) {
-    return Direction.getFacingFromVector(
-        (float) (placer.getPosX() - pos.getX()),
-        (float) (placer.getPosY() - pos.getY()),
-        (float) (placer.getPosZ() - pos.getZ()));
+    this.setDefaultState(
+        this.getStateContainer().getBaseState().with(BlockStateProperties.FACING, Direction.SOUTH));
   }
 
   @Override
@@ -34,15 +28,11 @@ public class BlockDiamondGenerator extends Block {
     builder.add(BlockStateProperties.FACING);
   }
 
+  @Nullable
   @Override
-  public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state,
-      @Nullable LivingEntity placer, ItemStack stack) {
-    if (placer != null) {
-      worldIn.setBlockState(
-          pos,
-          state.with(BlockStateProperties.FACING, getFacingFromPlacer(pos, placer)),
-          2);
-    }
+  public BlockState getStateForPlacement(BlockItemUseContext context) {
+    return this.getDefaultState()
+        .with(BlockStateProperties.FACING, context.getNearestLookingDirection().getOpposite());
   }
 
   @Override
--- src/main/java/com/ropoocha/moddilities/tileentities/TileDiamondGenerator.java
+++ src/main/java/com/ropoocha/moddilities/tileentities/TileDiamondGenerator.java
@@ -1,11 +1,24 @@
 package com.ropoocha.moddilities.tileentities;
 
 import com.ropoocha.moddilities.registries.RegistryTileEntities;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class TileDiamondGenerator extends TileEntity implements ITickableTileEntity {
+
+  private ItemStackHandler handler;
 
   public TileDiamondGenerator(final TileEntityType<?> type) {
     super(type);
@@ -17,8 +30,40 @@ public class TileDiamondGenerator extends TileEntity implements ITickableTileEnt
 
   @Override
   public void tick() {
-    if (world.isRemote) {
-      System.out.println("TileDiamondGenerator.tick");
+  }
+
+  @Override
+  public void read(BlockState state, CompoundNBT tag) {
+    CompoundNBT invTag = tag.getCompound("inv");
+    getHandler().deserializeNBT(invTag);
+    super.read(state, tag);
+  }
+
+  @Override
+  public CompoundNBT write(CompoundNBT tag) {
+    CompoundNBT compound = getHandler().serializeNBT();
+    tag.put("inv", compound);
+    return super.write(tag);
+  }
+
+  private ItemStackHandler getHandler() {
+    if (handler == null) {
+      handler = new ItemStackHandler(1) {
+        @Override
+        public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+          return stack.getItem() == Items.DIAMOND;
+        }
+      };
     }
+    return handler;
+  }
+
+  @Nonnull
+  @Override
+  public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+    if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+      return LazyOptional.of(() -> (T) getHandler());
+    }
+    return super.getCapability(cap, side);
   }
 }
